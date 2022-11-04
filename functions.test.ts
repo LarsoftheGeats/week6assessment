e59a63a592109c04f1175cb6ee5f698b19ee62c0
Jonathan Nielsen <larsnielsenj@gmail.com>
2022-11-04 12:03:26 -0600

    completed first test suite

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,34 @@
 const {shuffleArray} = require('./utils')
 
+
 describe('shuffleArray should', () => {
-    // CODE HERE
+    
+    test('Array returned an Array' , async () => {
+        let arr=[0,1,2,3,4,8,9,10]
+    
+        let returnArr = shuffleArray(arr)
+        expect(Array.isArray(returnArr))
+    })
+    
+    test('Array returned an Array of the same length' , async () => {
+        let arr=[0,1,2,3,4,8,9,10]
+    
+        let returnArr = shuffleArray(arr)
+        expect(arr.length).toBe(returnArr.length)
+    })
+    test('Array returned all the elements of the original array' , async () => {
+        let arr=[0,1,2,3,4,8,9,10]
+    
+        let returnArr = shuffleArray(arr)
+        let allPresent = true;
+        for (let i =0; i < arr.length; i++) {
+            if (!(returnArr.includes(arr[i]))){
+                allPresent=false;
+            }// if any element isn't in the new array the flag is set to false
+        }
+        expect(allPresent).toBeTruthy()
+    })
+    
+    
+
 })
